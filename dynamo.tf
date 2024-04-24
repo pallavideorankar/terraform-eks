0628c4b0301d9914a32c899cc7a85ca2602c6684
sarbajit <sarba.jit.das433@gmail.com>
2024-04-24 16:20:26 +0000

    adding dynamodb table to lock tfstate file in S3 and store hash key

---
+++ dynamo.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  billing_mode = "PAY_PER_REQUEST"  # Optional
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+     Name = "Terraform Lock Table"
+   }
+}
