433cae3852293de9874e440686e2922ae5a59d0a
sarbajit <sarba.jit.das433@gmail.com>
2024-04-24 16:22:20 +0000

    associating dynamodb table details with S3 backend

--- backend.tf
+++ backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "futuiontech-eks-terraform"
     key    = ".terraform.tfstate"
     region = "ap-south-1"
+    encrypt = true
+    dynamodb_table = "terraform-state-lock-dynamo"
   }
 }
 
