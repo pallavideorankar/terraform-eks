4c7ea02a887587d33c3188bcd70e6a4b2e088a84
sarbajit <sarba.jit.das433@gmail.com>
2024-04-24 17:05:20 +0000

    commenting out billing_mode as read & write capacity can't be set when using PAY_PER_REQUEST mode

--- dynamo.tf
+++ dynamo.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   name           = "terraform-state-lock-dynamo"
   hash_key       = "LockID"
-  billing_mode   = "PAY_PER_REQUEST" # Optional
+  # billing_mode   = "PAY_PER_REQUEST"
   read_capacity  = 20
   write_capacity = 20
 
