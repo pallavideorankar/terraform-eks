f8fb60b262e93a97053f74534900507e12b0b78b
sarbajit <sarba.jit.das433@gmail.com>
2024-04-29 16:23:29 +0000

    updating backend.tf file

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = ".terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock-dynamo"
+    dynamodb_table = "terraform-state-lock-dynamodb"
   }
 }
 
